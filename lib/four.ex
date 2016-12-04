27fc69e769a65c8f90c86c28718691400645ad31
Mitchell Henke <mitchh23@gmail.com>
2016-12-04 11:04:01 -0600

    day four part two

--- lib/four.ex
+++ lib/four.ex
@@ -3,8 +3,7 @@ defmodule Adventofcode2016.Four do
   @codes File.read!("./lib/four.txt") |> String.trim |> String.split("\n") 
   |> Enum.map(fn(x) -> Regex.named_captures(@regex, x) end)
   |> Enum.map(fn(x) ->
-    Map.put(x, "characters", String.replace(x["characters"], "-", ""))
-    |> Map.put("checksum", String.replace(x["checksum"], ["[", "]"], ""))
+    Map.put(x, "checksum", String.replace(x["checksum"], ["[", "]"], ""))
     |> Map.put("number", String.to_integer(x["number"]))
   end)
 
@@ -25,6 +24,7 @@ defmodule Adventofcode2016.Four do
     |> Enum.reduce(%{}, fn(letter, acc) ->
       Map.update(acc, letter, 1, &(&1 + 1))
     end)
+    |> Map.delete("-")
     |> Map.to_list
     |> Enum.sort_by(fn({letter, count}) -> [-1 * count, letter] end)
     |> Enum.take(5)
@@ -33,4 +33,50 @@ defmodule Adventofcode2016.Four do
     end)
     |> Enum.join()
   end
+
+  def four_two do
+    Enum.map(@codes, fn(x) ->
+      Map.put(x, "shifted", shift_string(x["characters"], x["number"]))
+    end)
+    |> Enum.filter(fn(x) -> String.contains?(x["shifted"], "pole") end)
+  end
+
+  def shift_string(string, number) do
+    String.graphemes(string)
+    |> Enum.map(fn(char) ->
+      Enum.reduce(1..number, char, fn(_, acc) ->
+        shift(acc)
+      end)
+    end)
+    |> Enum.join
+  end
+
+  def shift(" "), do: " "
+  def shift("-"), do: " "
+  def shift("a"), do: "b"
+  def shift("b"), do: "c"
+  def shift("c"), do: "d"
+  def shift("d"), do: "e"
+  def shift("e"), do: "f"
+  def shift("f"), do: "g"
+  def shift("g"), do: "h"
+  def shift("h"), do: "i"
+  def shift("i"), do: "j"
+  def shift("j"), do: "k"
+  def shift("k"), do: "l"
+  def shift("l"), do: "m"
+  def shift("m"), do: "n"
+  def shift("n"), do: "o"
+  def shift("o"), do: "p"
+  def shift("p"), do: "q"
+  def shift("q"), do: "r"
+  def shift("r"), do: "s"
+  def shift("s"), do: "t"
+  def shift("t"), do: "u"
+  def shift("u"), do: "v"
+  def shift("v"), do: "w"
+  def shift("w"), do: "x"
+  def shift("x"), do: "y"
+  def shift("y"), do: "z"
+  def shift("z"), do: "a"
 end
