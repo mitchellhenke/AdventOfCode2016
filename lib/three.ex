584c3c87bdb99cd552d060fc4f0e90645c995769
Mitchell Henke <mitchh23@gmail.com>
2016-12-04 08:51:08 -0600

    day three part two

--- lib/three.ex
+++ lib/three.ex
@@ -6,6 +6,18 @@ defmodule Adventofcode2016.Three do
     Enum.filter(@triangles, &valid_triangle?/1)
   end
 
+  def three_two do
+   Enum.chunk(@triangles, 3)
+   |> Enum.reduce([], fn([one, two, three], acc) ->
+     first_triangle = [Enum.at(one, 0), Enum.at(two, 0), Enum.at(three, 0)]
+     second_triangle = [Enum.at(one, 1), Enum.at(two, 1), Enum.at(three, 1)]
+     third_triangle = [Enum.at(one, 2), Enum.at(two, 2), Enum.at(three, 2)]
+
+     [first_triangle | [second_triangle | [third_triangle | acc]]]
+   end)
+   |> Enum.filter(&valid_triangle?/1)
+  end
+
   def valid_triangle?([a, b, c]) when a + b <= c, do: false
   def valid_triangle?([a, b, c]) when c + a <= b, do: false
   def valid_triangle?([a, b, c]) when c + b <= a, do: false
