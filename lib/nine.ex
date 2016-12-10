951400ea1e49cedb27cef5d797fa42f60ba9f61a
Mitchell Henke <mitchh23@gmail.com>
2016-12-10 10:03:27 -0600

    day 9 part two

--- lib/nine.ex
+++ lib/nine.ex
@@ -5,9 +5,17 @@ defmodule Adventofcode2016.Nine do
     decompress(@text)
   end
 
+  def nine_two do
+    decompress_two(@text)
+  end
+
   def decompress(string) do
     String.graphemes(string)
     |> do_decompress("")
+  end
+
+  def decompress_two(string) do
+    do_decompress_two(string, 0)
   end
 
   def do_decompress([], acc), do: acc
@@ -27,5 +35,21 @@ defmodule Adventofcode2016.Nine do
 
   def do_decompress([character | rest], acc) do
     do_decompress(rest, acc <> character)
+  end
+
+  defp do_decompress_two(raw, count)
+
+  defp do_decompress_two("", count), do: count
+
+  defp do_decompress_two(<<"(", rest::binary>>, count) do
+    [counts, rest] = String.split(rest, ")", parts: 2)
+    [num, times] = counts |> String.split("x") |> Enum.map(&String.to_integer/1)
+    {compressed, rest} = String.split_at(rest, num)
+
+    do_decompress_two(String.duplicate(compressed, times) <> rest, count)
+  end
+
+  defp do_decompress_two(<<_::binary-size(1), rest::binary>>, count) do
+    do_decompress_two(rest, count + 1)
   end
 end
